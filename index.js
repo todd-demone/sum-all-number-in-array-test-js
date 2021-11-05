b1eb0f67cb7dfadb8c420a28c5d843fcacbe11f1
Todd Demone <tdemone1@gmail.com>
2021-11-05 20:30:12 +0000

    preserving my console.log's in case I need to refer to them in the future

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = function(array) {
+  let sum = 0;
+  for (const val of array) {
+    // console.log(`sum: ${sum}`);
+    // console.log(`val: ${val}`);
+    // console.log(`is it an array? ${Array.isArray(val)}`);
+    if (Array.isArray(val)) {
+      // console.log(`calling sumItems again...`);
+      sum += sumItems(val);
+    } else {
+      // console.log(`adding val to sum...`);
+      sum += val;
+      // console.log(`sum is now: ${sum}`);
+      // console.log(`---------------------`);
+    }
+  }
+  return sum;
+};
 
+// console.log(sumItems([1, [1, 1], 1]));
 module.exports = sumItems;
